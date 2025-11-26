5b230a7819723bfd288d1d3b78bca9680e9c6270
Valentyn Levytskyi <valevytskyi@icloud.com>
2025-11-26 23:10:06 +0100

    landing: Add deploy scripts

--- struggleforum-landing/next.config.ts
+++ struggleforum-landing/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
+const repoName = "struggleforum";
+const githubUser = "va13k";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
