a9835b6cdc605296d61a1162c767b0fa0f96f4d8
Valentyn Levytskyi <valevytskyi@icloud.com>
2025-11-26 23:36:18 +0100

    Updates

--- _next/static/qFW6DeuEWgHtyK5chA2dI/_buildManifest.js
+++ _next/static/qFW6DeuEWgHtyK5chA2dI/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/struggleforum//_next/:path+",
-        "destination": "/struggleforum/_next/:path+"
+        "source": "/./_next/:path+",
+        "destination": "/_next/:path+"
       }
     ],
     "fallback": []
