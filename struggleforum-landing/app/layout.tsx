e52d9a84c74c174333a96440c8e136ba5cc2c300
Valentyn Levytskyi <valevytskyi@icloud.com>
2025-11-29 15:00:43 +0100

    feature(landing): Add Footer component + Fonts

--- struggleforum-landing/app/layout.tsx
+++ struggleforum-landing/app/layout.tsx
@@ -1,13 +1,27 @@
 import "./global.css";
 import { ReactNode } from "react";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="min-h-screen flex flex-col items-center pt-[70px]">
         <Header />
-        <main>{children}</main>
+        {children}
+        <Footer />
       </body>
     </html>
   );
