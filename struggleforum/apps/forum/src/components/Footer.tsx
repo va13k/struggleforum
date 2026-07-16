6679817fdbaf742fa61fef7848a64ff10ecd13f4
Valentyn Levytskyi <valevytskyi@icloud.com>
2026-07-16 23:30:45 +0200

    feat(forum): Implement simple Footer component

---
+++ struggleforum/apps/forum/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-slate-800/80 px-5 py-4 text-center text-sm text-slate-400 shadow-[0_-2px_10px_rgba(0,0,0,0.3)]">
+      <p>StruggleForum</p>
+    </footer>
+  );
+}
